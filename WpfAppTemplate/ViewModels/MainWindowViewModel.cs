39f53c64d59c66b21bac97f0823dd6a80e3a0954
SOU MEIRIN <824099499@qq.com>
2024-09-19 21:42:15 +0900

    アプリをCloseの機能の追加

---
+++ WpfAppTemplate/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfAppTemplate.ViewModels
+{
+    class MainWindowViewModel
+    {
+        public DelegateCommand CloseAppCommand { get; set; }
+
+        public MainWindowViewModel()
+        {
+            CloseAppCommand = new DelegateCommand(ExcuteCloseApp);
+        }
+
+        private void ExcuteCloseApp()
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
